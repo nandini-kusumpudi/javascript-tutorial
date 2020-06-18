c8127640c1557fb4d49a6be9252ff2169101204d
Chandra Manthina <cmant47@outlook.com>
2020-06-18 12:54:32 +0100

    added variable example data

--- src/variables.js
+++ src/variables.js
@@ -2,3 +2,10 @@ var x = 10;
 var y = 5;
 var z = x + y;
 console.log(z);
+
+var firstName = 'Nandini';
+var age = 25;
+var lastName = 'Manthina';
+var space = ' ';
+
+console.log(firstName + ' ' + age + ' ' + lastName);
