2b9c7162337fbfe6197ad52a712fb54429b4fc57
Chandra Manthina <cmant47@outlook.com>
2020-06-19 17:05:03 +0100

    added example var mutation and type coerction

---
+++ src/3.variable-mutation-type-coerction.js
@@ -0,0 +1,22 @@
+var firstName = 'John'; 
+var age = '28';
+
+//type coerction
+console.log(firstName +''+age);
+
+var job, isMarried;
+job ='Teacher';
+isMarried= false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' .Is he married ' + isMarried);
+
+
+//variable mutation
+
+age='twenty eight';
+job='driver';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' .Is he married ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + '' + lastName);
