e3f63dfe73448ace7cc4604bcb1a045961758214
Chandra Manthina <cmant47@outlook.com>
2020-06-19 15:32:33 +0100

    added data types examples

---
+++ src/2.datatypes.js
@@ -0,0 +1,14 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';/*String*/
+var age = 28 ;/*number*/
+
+var fullAge = true;/*boolean*/
+console.log(fullAge);
+
+var job;/*undefined*/
+console.log(job);
+
+job = 'Teacher';/*null*/
+console.log(job);
